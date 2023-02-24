826d31ca9c913d4947b05c6ec98309791677b103
victor canedo <victorcanedo1@hotmail.com>
2023-02-24 16:40:59 -0300

    Yelp front changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = %w[chinese italian japanese french belgian]
+
   validates :name, :address, :category, presence: true
-  validates :category, acceptance: { accept: %w[chinese italian japanese french belgian] }
+  validates :category, acceptance: { accept: CATEGORY }
 
   has_many :reviews, dependent: :destroy
 end
